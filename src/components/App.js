2f6f504d059382b44bbda5e08496ffe1a699e48b
Ajaykarki13 <ajaymum698@gmail.com>
2023-04-11 21:15:13 +0530

    soln

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,28 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React,{useState,useEffect} from 'react';
+//import marked from 'marked';
+import ReactMarkdown from 'react-markdown';
+
+function App()
+{
+    const[write,setWrite] = useState('');
+{/* const[html,setHtml] = useState('');
+useEffect(()=>{
+    const output = marked(write);
+    setHtml(output)
+}) */}
+  
+
+return(
+    <>
+    <div className='writedown'>
+        <textarea onChange={(e)=>setWrite(e.target.value)} type='text'  value={write} >
+            </textarea> 
+    </div>
+    <ReactMarkdown className='output' source={write} />
+   {/*<div className='output' dangerouslySetInnerHTML={{ __html: html }}></div> */} 
+    </>
+)
+
+}
+export default App;
